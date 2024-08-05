92a5598a3c4fcafacf4f38f00ae05760e1e0282d
ShakilOfficials <mrshakilhossainofficial@gmail.com>
2024-08-05 10:25:37 +0600

    implement redux

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,16 +1,33 @@
-import Button from "./Button";
-import Count from "./Count";
+import { useState } from "react";
 
-const Counter = ({ id, count, increment, decrement }) => {
+export default function Counter() {
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount((prevCount) => prevCount + 1);
+  };
+
+  const decrement = () => {
+    setCount((prevCount) => prevCount - 1);
+  };
+
   return (
     <div className="p-4 h-auto flex flex-col items-center justify-center space-y-5 bg-white rounded shadow">
-      <Count count={count} />
+      <div className="text-2xl font-semibold">{count}</div>
       <div className="flex space-x-3">
-        <Button handler={() => increment(id)}>Increment</Button>
-        <Button handler={() => decrement(id)}>Decrement</Button>
+        <button
+          className="bg-indigo-400 text-white px-3 py-2 rounded shadow"
+          onClick={increment}
+        >
+          Increment
+        </button>
+        <button
+          className="bg-red-400 text-white px-3 py-2 rounded shadow"
+          onClick={decrement}
+        >
+          Decrement
+        </button>
       </div>
     </div>
   );
-};
-
-export default Counter;
+}
---
+++ src/redux/store.js
@@ -0,0 +1,10 @@
+import { configureStore } from "@reduxjs/toolkit";
+import CounterReducer from "./counter/counterReducer";
+
+const store = configureStore({
+  reducer: {
+    counter: CounterReducer,
+  },
+});
+
+export default store;
