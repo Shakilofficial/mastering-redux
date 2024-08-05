904297b6be96625c39354feadf6280a680814e55
ShakilOfficials <mrshakilhossainofficial@gmail.com>
2024-08-05 08:41:48 +0600

    nested counter with react states

---
+++ src/components/Button.jsx
@@ -0,0 +1,13 @@
+const Button = ({ children, handler }) => {
+  return (
+    <button
+      className="bg-indigo-400 text-white px-3 py-2 rounded shadow"
+      id="increment"
+      onClick={handler}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
---
+++ src/components/Count.jsx
@@ -0,0 +1,9 @@
+const Count = ({ count }) => {
+  return (
+    <div className="px-5 py-3 rounded-full bg-blue-200">
+      <h1 className="text-2xl font-semibold">{count}</h1>
+    </div>
+  );
+};
+
+export default Count;
