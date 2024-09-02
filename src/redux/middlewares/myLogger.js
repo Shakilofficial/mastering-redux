14ed54ff027f1adb4b7f4129fc9bca2e75e595eb
ShakilOfficials <mrshakilhossainofficial@gmail.com>
2024-09-03 03:22:45 +0600

    add middlewares

---
+++ src/redux/middlewares/myLogger.js
@@ -0,0 +1,14 @@
+import rootReducer from "../rootReducer";
+
+//create our first middleware
+const myLogger = (store) => (next) => (action) => {
+    console.log(`Action: ${JSON.stringify(action)}`);
+    console.log(`Before:${JSON.stringify(store.getState())}`);
+  
+    const upcomingState = [action].reduce(rootReducer, store.getState());
+    console.log(`Upcoming:${JSON.stringify(upcomingState)}`);
+    //pass the action
+    return next(action);
+};
+  
+export default myLogger;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
+// import myLogger from "./middlewares/myLogger";
+import { composeWithDevTools } from "@redux-devtools/extension";
+import logger from "redux-logger";
 import rootReducer from "./rootReducer";
 
 const store = configureStore({
   reducer: rootReducer,
+  middleware: composeWithDevTools((getDefaultMiddleware) => getDefaultMiddleware().concat(logger)),
+  // Add custom middleware
 });
-
 export default store;
