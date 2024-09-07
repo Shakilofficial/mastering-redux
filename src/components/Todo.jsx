48ca1f3ae7e86ac377da75814c3dddfb326ebb48
ShakilOfficials <mrshakilhossainofficial@gmail.com>
2024-09-07 11:00:38 +0600

    update it with redux thunk and dispatch action with thunks

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,26 +1,28 @@
 import { useDispatch } from "react-redux";
 import cancelImage from "../assets/images/cancel.png";
-import { colorSelected, deleted, toggled } from "../redux/todos/actions";
+import deleteTodo from "../redux/todos/thunk/deleteTodo";
+import updateColor from "../redux/todos/thunk/updateColor";
+import updateStatus from "../redux/todos/thunk/updateStatus";
 
 const Todo = ({ todo }) => {
   const { text, id, color, completed } = todo;
   const dispatch = useDispatch();
 
   const handleStatusChange = (todoId) => {
-    dispatch(toggled(todoId));
+    dispatch(updateStatus(todoId, completed));
   };
   const handleColorChange = (todoId, color) => {
-    dispatch(colorSelected(todoId, color));
-};
-
-const handleDelete = (todoId) => {
-    dispatch(deleted(todoId));
+    dispatch(updateColor(todoId, color));
   };
-  
+
+  const handleDelete = (todoId) => {
+    dispatch(deleteTodo(todoId));
+  };
+
   return (
     <div className="flex justify-start items-center p-2 hover:bg-gray-100 hover:transition-all space-x-4 border-b border-gray-400/20 last:border-0">
       <div
-        className={`rounded-full bg-white border-2 w-5 h-5 flex flex-shrink-0 justify-center items-center mr-2 ${
+        className={`relative rounded-full bg-white border-2 w-5 h-5 flex flex-shrink-0 justify-center items-center mr-2 ${
           completed
             ? "border-green-500 focus-within:border-green-500"
             : "border-gray-400"
