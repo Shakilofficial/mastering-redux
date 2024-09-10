ef3c8f6d1f4494b169c378902ec844dd83bdd1f3
ShakilOfficials <mrshakilhossainofficial@gmail.com>
2024-09-10 17:57:33 +0600

    added dynamic Counter

---
+++ rtk/app/store.js
@@ -0,0 +1,12 @@
+const { configureStore } = require("@reduxjs/toolkit");
+const counterReducer = require("../features/counter/counterSlice");
+const dynamicCounterReducer = require("../features/dynamicCounter/dynamicCounterSlice");
+// configure store
+const store = configureStore({
+  reducer: {
+    counter: counterReducer,
+    dynamicCounter: dynamicCounterReducer,
+  },
+});
+
+module.exports = store;
