ac6f17506ce197ade6c48460314bba102f853ca3
ShakilOfficials <mrshakilhossainofficial@gmail.com>
2024-09-10 18:16:33 +0600

    added dynamic Counter

--- rtk/index.js
+++ rtk/index.js
@@ -9,15 +9,15 @@ console.log(`Initial State: ${JSON.stringify(store.getState())}`);
 
 // subscribe to state changes
 store.subscribe(() => {
-  console.log(store.getState());
+//   console.log(store.getState());
 });
 
 // dispatch actions
-// store.dispatch(counterActions.increment());
+store.dispatch(counterActions.increment());
 
-// store.dispatch(counterActions.increment());
+store.dispatch(counterActions.increment());
 
-// store.dispatch(counterActions.decrement());
+store.dispatch(counterActions.decrement());
 
 store.dispatch(dynamicCounterActions.increment(3));
 
