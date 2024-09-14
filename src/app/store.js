d41b51c8803873d39272b22998bbecab478b3fdf
ShakilOfficials <mrshakilhossainofficial@gmail.com>
2024-09-14 11:30:58 +0600

    add tags features and showing it tags ui components

--- src/app/store.js
+++ src/app/store.js
@@ -1,7 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
+import tagReducer from "../features/tags/tagsSlice";
 import videoReducer from "../features/videos/videosSlice";
 export const store = configureStore({
   reducer: {
     videos: videoReducer,
+    tags: tagReducer,
   },
 });
