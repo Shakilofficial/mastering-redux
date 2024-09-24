277335788247ee76c713b63f9e70f42d40aac8ea
ShakilOfficials <mrshakilhossainofficial@gmail.com>
2024-09-24 11:21:41 +0600

    Complete Project

--- src/components/Transactions/Transaction.js
+++ src/components/Transactions/Transaction.js
@@ -1,16 +1,32 @@
+import { useDispatch } from "react-redux";
 import deleteImage from "../../assets/images/delete.svg";
 import editImage from "../../assets/images/edit.svg";
+import {
+  editActive,
+  removeTransaction,
+} from "../../features/transactions/transactionsSlice";
 
-export default function Transaction() {
+export default function Transaction({ transaction }) {
+  const { name, amount, type, id } = transaction || {};
+
+  const dispatch = useDispatch();
+  const handleEdit = () => {
+    dispatch(editActive(transaction));
+  };
+
+  const handleDelete = () => {
+    dispatch(removeTransaction(id));
+  };
+
   return (
-    <li className="transaction income">
-      <p>Earned this month</p>
+    <li className={`transaction ${type}`}>
+      <p>{name}</p>
       <div className="right">
-        <p>৳ 100</p>
-        <button className="link">
+        <p>৳ {amount}</p>
+        <button onClick={handleEdit} className="link">
           <img alt="Edit" className="icon" src={editImage} />
         </button>
-        <button className="link">
+        <button onClick={handleDelete} className="link">
           <img alt="Delete" className="icon" src={deleteImage} />
         </button>
       </div>
--- src/components/Transactions/Transactions.js
+++ src/components/Transactions/Transactions.js
@@ -1,14 +1,38 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchTransactions } from "../../features/transactions/transactionsSlice";
+import Loading from "../Loading";
 import Transaction from "./Transaction";
 
 export default function Transactions() {
+  const dispatch = useDispatch();
+  const { transactions, isLoading, isError, error } = useSelector(
+    (state) => state.transaction
+  );
+
+  useEffect(() => {
+    dispatch(fetchTransactions());
+  }, [dispatch]);
+
+  //decide what to render
+  let content = null;
+  if (isLoading) content = <Loading />;
+  if (!isLoading && isError)
+    content = <p className="error">There was an error occured</p>;
+  if (!isError && !isLoading && transactions?.length > 0) {
+    content = transactions.map((transaction) => (
+      <Transaction key={transaction.id} transaction={transaction} />
+    ));
+  }
+  if (!isError && !isLoading && transactions?.length === 0) {
+    content = <p>No Transaction Found</p>;
+  }
   return (
     <>
       <p className="second_heading">Your Transactions:</p>
 
       <div className="conatiner_of_list_of_transactions">
-        <ul>
-          <Transaction />
-        </ul>
+        <ul>{content}</ul>
       </div>
     </>
   );
